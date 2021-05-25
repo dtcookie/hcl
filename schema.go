bb56e4b65c8b7e7b963a0b018ab0d9c43260fed0
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2021-05-25 12:09:35 +0200

    Update schema.go

--- schema.go
+++ schema.go
@@ -1,17 +1,22 @@
 package hcl
 
 type Schema struct {
-	Type        ValueType
-	Description string
-	Optional    bool
-	MaxItems    int
-	MinItems    int
-	Sensitive   bool
-	Elem        interface{}
-	Default     interface{}
-	Required    bool
-	Deprecated  string
-	Computed    bool
+	Type          ValueType
+	Description   string
+	Optional      bool
+	MaxItems      int
+	MinItems      int
+	Sensitive     bool
+	Elem          interface{}
+	Default       interface{}
+	Required      bool
+	Deprecated    string
+	Computed      bool
+	ConflictsWith []string
+	ExactlyOneOf  []string
+	AtLeastOneOf  []string
+	RequiredWith  []string
+	ForceNew      bool
 }
 
 type ValueType int
